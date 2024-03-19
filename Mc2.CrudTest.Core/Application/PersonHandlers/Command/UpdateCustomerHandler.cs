89e9ad562d2a11685862993c0f42cc5efeec6466
Vahid Moshtaghian <vahid.moshtaghian@hotmail.com>
2024-03-19 19:40:19 +0330

    add validations to the update custoemr

--- Mc2.CrudTest.Core/Application/PersonHandlers/Command/UpdateCustomerHandler.cs
+++ Mc2.CrudTest.Core/Application/PersonHandlers/Command/UpdateCustomerHandler.cs
@@ -2,6 +2,7 @@
 using Mc2.CrudTest.Core.Domain.Dto.PersonModels;
 using Mc2.CrudTest.Core.Domain.Entity;
 using Mc2.CrudTest.Core.Domain.Exceptions;
+using Mc2.CrudTest.Core.Domain.Utility;
 using MediatR;
 
 namespace Mc2.CrudTest.Core.Application.PersonHandlers.Command;
@@ -17,6 +18,11 @@ public class UpdateCustomerHandler : IRequestHandler<UpdateCustomerCommand>
 
     public async Task Handle(UpdateCustomerCommand request, CancellationToken cancellationToken)
     {
+        if (!request.Email.IsValidEmail())
+            throw new EmailValidationException();
+        if (!request.PhoneNumber.IsValidPhone())
+            throw new PhoneValidationException();
+
         var entity = await _context.Set<Customer>()
             .FindAsync(request.Id, cancellationToken)
             ?? throw new NotFoundException();
