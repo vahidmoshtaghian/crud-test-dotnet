597c5d912d13e9cf2884ec5ea562fca4b1118d48
Vahid Moshtaghian <vahid.moshtaghian@hotmail.com>
2024-03-19 11:01:40 +0330

    Add project files.

---
+++ Mc2.CrudTest.AcceptanceTests/Hooks/Hook.cs
@@ -0,0 +1,17 @@
+namespace Mc2.CrudTest.AcceptanceTests.Hooks;
+
+[Binding]
+public class Hooks
+{
+    [Before]
+    public void Before()
+    {
+
+    }
+
+    [After]
+    public void After()
+    {
+
+    }
+}
